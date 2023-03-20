13597845be36aa1375a674cfe3d846d11ebd8980
Barbazen <romainbarbason2812@gmail.com>
2023-03-21 00:42:39 +0100

    Trucs DNS en plus

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ import pyshark
 
 cap = pyshark.FileCapture('Packets/Edurom/Cree+Ferme-meet-instantané-no-micro-no-camera- 20 ⁄ 03.pcapng')
 
-DNS_Resolved = 0
-DNS_RequestsType = {}
+DNS_Resolved = []
+DNS_RequestsType = {1: 0, 28: 0}
 tempDNS = []
 
 for packet in cap:
     if 'DNS' in packet:
-        if packet.flags == 0x8180:
-            DNS_Resolved += 1
-            #Notez entreprise
+        if str(packet.dns.flags) == "0x8180":
+            DNS_Resolved.append(packet.frame_info.time_relative)  # (y a aussi time_epoch et time)
+            # Notez entreprise
             if packet.dns.flags_authoritative != 0:
-                pass
-                #Pas encore de paquets avec 1 (avec les 2 paquets)
-            if packet.dns.qry_type not in tempDNS:
-                tempDNS.append(packet.dns.qry_type)
-            #Record additionnel??
-            #Comportements DNS inattendus??
+                print("Hello!")
+                # Pas encore de paquets avec 1 (avec les 2 paquets)
+            if int(packet.dns.qry_type) == 1:
+                DNS_RequestsType.update({1: DNS_RequestsType.get(1) + 1})
+            else:
+                DNS_RequestsType.update({28: DNS_RequestsType.get(28) + 1})
+            # Record additionnel??
+            # Comportements DNS inattendus??
 
-    #TODO Serveurs autoritatifs
-
+print("Resolved: ", len(DNS_Resolved))
+print(DNS_RequestsType)
