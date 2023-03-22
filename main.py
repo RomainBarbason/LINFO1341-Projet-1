fe289f5af5432d098acd7160f6e05e8f70ba8e17
Barbazen <romainbarbason2812@gmail.com>
2023-03-22 15:15:41 +0100

    Peter griffin robot

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ cap = pyshark.FileCapture('Packets/Edurom/Cree+Ferme-meet-instantané-no-micro-n
 
 DNS_Resolved = []
 DNS_RequestsType = {1: 0, 28: 0}
+QUIC_Versions = {}
 tempDNS = []
+print(str(cap[27].quic.version) in QUIC_Versions)
+i = 1
 
 for packet in cap:
     if 'DNS' in packet:
@@ -12,7 +15,7 @@ for packet in cap:
             DNS_Resolved.append(packet.frame_info.time_relative)  # (y a aussi time_epoch et time)
             # Notez entreprise
             if packet.dns.flags_authoritative != 0:
-                print("Hello!")
+                print(i)
                 # Pas encore de paquets avec 1 (avec les 2 paquets)
             if int(packet.dns.qry_type) == 1:
                 DNS_RequestsType.update({1: DNS_RequestsType.get(1) + 1})
@@ -20,6 +23,13 @@ for packet in cap:
                 DNS_RequestsType.update({28: DNS_RequestsType.get(28) + 1})
             # Record additionnel??
             # Comportements DNS inattendus??
+    # if 'QUIC' in packet:
+    #     if packet.quic str(packet.quic.version) in QUIC_Versions:
+    #         QUIC_Versions.update({str(packet.quic.version): QUIC_Versions.get(str(packet.quic.version)) + 1})
+    #     else:
+    #         QUIC_Versions.update({str(packet.quic.version): 1})
+    # i += 1
+
 
 print("Resolved: ", len(DNS_Resolved))
 print(DNS_RequestsType)
